5596fdfe2aa8f84ac8a3c4bd64e491da94017681
Fei.Hong <1035523318@qq.com>
2023-05-06 18:38:26 +0800

    Final

--- main.py
+++ main.py
@@ -1,190 +1,86 @@
-import qiniu_api
-import time
-import numpy as np
+import tkinter as tk
+from tkinter import ttk
+import json
+from PIL import Image, ImageTk
+from qiniu_api import *
 
-class Camera7niu(FlowBase):
-    def __init__(self, app_setting_filename):
-        super().__init__(None, app_setting_filename)
 
-        self.app_key = self.app_setting.get_key('app_key')
-        self.app_secret = self.app_setting.get_key('app_secret')
-        self.stream_id = self.app_setting.get_key('stream_id')
-        self.namespace_id = self.app_setting.get_key('namespace_id')
-        gbid = self.stream_id
-        brand = '未知'
-        try:
-            h, r = qiniu_api.listNamespacesInfo(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-            brand = get_dict_key(r, 'vendor', default='不可用')
-        except Exception as ex:
-            exec(ERROR_PRINT)
+class CameraApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Live streaming service')
+        self.geometry('500x500')
+        self.resizable(False, False)
 
-        self.camera_info_for_next_stage = {
-            'brand': brand,
-            'model': 'Cloud',
-            'camsn': self.stream_id,
-            'fixsn': self.namespace_id,
-            'name': get_app_title_without_html(self.app_setting, marker='_'),
-            'sub_ext': '',
-            'url': '',
-            'extra': get_extra_dict(self.app_setting.get_key('extra'))
-        }
+        # Create text widget
+        self.result_text = tk.Text(self, height=23, width=60)
+        self.result_text.pack(side='top', pady=10)
 
-    def close_camera(self):
-        try:
-            h, r = qiniu_api.stopDevice(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-            if h['code'] == 200:
-                self.log('正确关闭设备')
-            else:
-                self.log(f"关闭设备错误：{get_dict_key(h, 'text_body', default='未知错误')}")
+        # Create button frames
+        button_frame1 = tk.Frame(self)
+        button_frame2 = tk.Frame(self)
 
-            h, r = qiniu_api.stopStreams(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-            if h['code'] == 200:
-                self.log('正确关闭推流')
-            else:
-                self.log(f"关闭推流错误：{get_dict_key(h, 'text_body', default='未知错误')}")
+        # Create buttons
+        self.start_device_button = ttk.Button(button_frame1, text='Start Device', command=self.start_device)
+        self.stop_device_button = ttk.Button(button_frame1, text='Stop Device', command=self.stop_device)
+        self.list_info_button = ttk.Button(button_frame2, text='List info', command=self.list_info)
+        self.start_live_button = ttk.Button(button_frame2, text='Start Live', command=self.start_live)
 
-        except Exception as ex:
-            exec(ERROR_PRINT)
+        # Pack buttons
+        self.start_device_button.pack(side='left', padx=5, pady=5)
+        self.stop_device_button.pack(side='right', padx=5, pady=5)
+        self.list_info_button.pack(side='left', padx=5, pady=5)
+        self.start_live_button.pack(side='right', padx=5, pady=5)
 
-    def capture(self, customer):
-        customer.send(None)
-        # 使能流
-        max_trial = 5
-        wait_time = 2
+        # Pack button frames
+        button_frame1.pack(side='top', pady=10)
+        button_frame2.pack(side='top', pady=10)
 
-        enable_stream_ok = False
-        start_stream_ok = False
-        goto_preset_ok = False
-        cap_ok = False
-        pic_ok = False
+        # Pack start button
 
-        start_cap_time = 0
-        end_cap_time = 0
+    def list_info(self):
+        headers, result = listNamespacesInfo(app_key, app_sec, ns_id, stm_id)
+        formatted_json = json.dumps(result, indent=4)
+        self.result_text.delete('1.0', tk.END)
+        self.result_text.insert(tk.END, formatted_json)
 
-        rtmp_url = ''
+    def start_device(self):
+        headers, result = startDevice(app_key, app_sec, ns_id, stm_id)
+        if result["code"] == 200:
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.END, 'Successful start!')
+        else:
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.END, 'Start Failed!')
 
-        for try_time in range(max_trial):
-            try:
-                h, r = qiniu_api.enableStreams(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-                if h['code'] != 200:
-                    self.log(f"使能流错误：{get_dict_key(h, 'text_body', default='未知错误')}")
-                    time.sleep(wait_time)
-                else:
-                    self.log(f"已经使能推流")
-                    enable_stream_ok = True
-                    break
-            except Exception as ex:
-                exec(ERROR_PRINT)
+    def stop_device(self):
+        headers, result = stopDevice(app_key, app_sec, ns_id, stm_id)
+        if result["code"] == 200:
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.END, 'Successful stop!')
+        else:
+            self.result_text.delete('1.0', tk.END)
+            self.result_text.insert(tk.END, 'Stop Failed!')
 
-        # 启动设备
-        if enable_stream_ok:
-            for try_time in range(max_trial):
-                try:
-                    h, r = qiniu_api.startDevice(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-                    if h['code'] != 200:
-                        self.log(f"设备启动错误：{get_dict_key(h, 'text_body', default='未知错误')}")
-                        time.sleep(wait_time)
-                    else:
-                        self.log(f"已经打开设备")
-                        start_stream_ok = True
-                        break
-                except Exception as ex:
-                    exec(ERROR_PRINT)
+    def show_image(self, image_path):
+        image = Image.open(image_path)
+        image = image.resize((400, 300))
+        photo = ImageTk.PhotoImage(image)
+        label = ttk.Label(self, image=photo)
+        label.image = photo
+        label.pack(side='top', pady=10)
 
-        # 回预置点
-        if start_stream_ok:
-            for try_time in range(max_trial):
-                try:
-                    h, r = qiniu_api.controlPresetBit(self.app_key, self.app_secret, self.namespace_id, self.stream_id,
-                                                      {'cmd': 'goto', 'name': '预置点1', 'presetId': 1})
-                    if h['code'] != 200:
-                        self.log(f"回预置点错误：{get_dict_key(h, 'text_body', default='未知错误')}")
-                        time.sleep(wait_time)
-                    else:
-                        self.log(f"相机回到预置点")
-                        goto_preset_ok = True
-                        time.sleep(wait_time * 2)
-                        break
-                except Exception as ex:
-                    exec(ERROR_PRINT)
+    def start_live(self):
+        start(app_key, app_sec, ns_id, stm_id)
+        self.result_text.delete('1.0', tk.END)
+        self.result_text.insert(tk.END, 'Live screen being acquired!')
 
-        # 截图
-        if goto_preset_ok:
-            # start_cap_time = int(time.time() - 2)
-            for try_time in range(max_trial):
-                try:
-                    h, r = qiniu_api.getStreamUrl(self.app_key, self.app_secret, self.namespace_id, self.stream_id)
-                    if h['code'] != 200:
-                        self.log(f"获取URL失败：{get_dict_key(h, 'text_body', default='未知错误')}")
-                        time.sleep(wait_time)
-                    else:
-                        rtmp_url = get_dict_key(r, 'playUrls.hls', default='')
-                        if rtmp_url != '':
-                            cap_ok = True
-                            self.log(f"获取图像地址")
-                            break
-                        else:
-                            self.log(f"获取URL失败：没有获取到URL", GREEN)
 
-                except Exception as ex:
-                    exec(ERROR_PRINT)
-
-        # 获取图像列表和获取图像
-        if cap_ok and rtmp_url != '':
-            for try_time in range(max_trial):
-                try:
-                    # print(rtmp_url)
-                    cap = cv.VideoCapture(rtmp_url)
-                    while cap.isOpened() is False and time.time() - start_cap_time < wait_time * max_trial:
-                        time.sleep(0.1)
-
-                    if cap.isOpened() is False:
-                        # 图像获取存在问题, 继续获取下一张图
-                        self.log('图像地址无法打开', RED)
-                        continue
-                    is_ok, frame = cap.read()
-                    if is_ok is False:
-                        continue
-                    while self.is_empty_image(frame) and time.time() - start_cap_time < wait_time * max_trial:
-                        is_ok, frame = cap.read()
-
-                    if self.is_empty_image(frame) is True:
-                        self.log('图像获取但是内容为空白', RED)
-                        continue
-                    else:
-                        self.log('图像正确获取！', GREEN)
-                        self.image = frame
-                        customer.send([self.camera_info_for_next_stage, {ORI_IMG: self.image}])
-                        break
-                except Exception as ex:
-                    exec(ERROR_PRINT)
-
-        self.close_camera()
-
-    def run(self):
-        customer = self.next_stage()
-        self.capture(customer)
-
-    def is_empty_image(frame):
-        try:
-            frame = cv.resize(frame, (1280, 720))
-            frame_64 = frame.astype(np.int64)
-            avg_0, avg_1, avg_2 = int(sum(sum(frame_64[:, :, 0]) / (1280 * 720))), \
-                                  int(sum(sum(frame_64[:, :, 1])) / (1280 * 720)), \
-                                  int(sum(sum(frame_64[:, :, 2])) / (1280 * 720))
-    
-            diff_0, diff_1, diff_2 = int(
-                sum(sum(abs(frame_64[:, :, 0] - avg_0))) / (1280 * 720)), \
-                                     int(sum(sum(abs(frame_64[:, :, 1] - avg_1))) / (
-                                             1280 * 720)), \
-                                     int(sum(sum(abs(frame_64[:, :, 2] - avg_2))) / (
-                                             1280 * 720))
-    
-            if diff_0 + diff_1 + diff_2 < 32:
-                return True
-            else:
-                return False
-        except:
-            return True
 if __name__ == '__main__':
-    Camera7niu()
+    app_key = 'RofRgvw0K2egvDxKz7n75RJCH_R49e-hQLk_z1kk'
+    app_sec = 'dMj5nG4ybGwzynCeoqopytKmsN2QDzg1QVnUF277'
+    stm_id = '31011500991320020843'
+    ns_id = 'qvs'
+
+    app = CameraApp()
+    app.mainloop()
